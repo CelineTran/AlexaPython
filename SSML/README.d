810cb0f9b9a99ca632ffb8cfe0bd51b827a4e149
CelineTran <celinetran7737@gmail.com>
2022-06-23 14:00:59 -0700

    Create README.d

---
+++ SSML/README.d
@@ -0,0 +1,9 @@
+## Alexa Speech Synthesis Markup Language 
+(https://developer.amazon.com/en-US/docs/alexa/custom-skills/speech-synthesis-markup-language-ssml-reference.html#break)[SSML]
+
+### Break 
+
+<speak>
+    There is a three second pause here <break time="3s"/>
+    then the speech continues.
+</speak>
